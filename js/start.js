af9c00f497ed89ee69d0a933c2151ef757d437ef
Vlad <vlad.mark-2013@mail.ru>
2019-10-09 23:55:44 +0300

    feature/v7.0 start page was upgrated

---
+++ js/start.js
@@ -0,0 +1,21 @@
+typeMSU();
+
+function hide_object(object, time=300){
+    object.animate({opacity: 0}, time, function(){
+    object.css("visibility", "hidden");
+});
+}
+
+setTimeout(() => {
+   hide_object($("#msu_background"));
+   typeCMC();
+   setTimeout(() => {
+     hide_object($("#cmc_background"));
+      typeNameVlad();
+      typeNameAnya();
+      typeNamePasha();
+
+      typeLeftDescription();
+      rightDescription();
+   }, 2500);
+}, 2000);
--- js/type_for_start_page.js
+++ js/type_for_start_page.js
@@ -23,6 +23,47 @@
     .tiType('<text style="color: #05eea8; font-size: 100%;">Pavel Shvetc</text>');
 }
 
-typeNameVlad();
-typeNameAnya();
-typeNamePasha();
+  function typeLeftDescription() {
+  $('#left_description').typeIt({
+    speed: 50,
+    autoStart: false,
+    html: true
+  })
+  .tiType('<text style="color: #fc5fa3; font-size: 140%;">Sub-poissonian</text>')
+  .tiPause(400)
+  .tiDelete(2)
+  .tiType('<text style="color: #fc5fa3;font-size: 140%;">an</text>')
+  .tiType('<text style="color: #fff"> and</text>')
+  .tiBreak()
+  .tiType('<text style="color: #05e6eebd"> super-poissonian</text>')
+  .tiType('<text style="color: #fff"> statistics</text>')
+
+}
+  function rightDescription() {
+  $('#right_description').typeIt({
+    speed: 50,
+    autoStart: false,
+    html: true
+  })
+    .tiType('<text style="color: #fff; font-size: 100%;">Billiard Modeling</text>');
+}
+
+  function typeMSU() {
+  $('#msu_background > .description').typeIt({
+    speed: 50,
+    autoStart: false,
+    html: true
+  })
+    .tiType('<text style="color: #000; font-size: 100%;">Moscow State University</text>');
+}
+  function typeCMC() {
+  $('#cmc_background > .description').typeIt({
+    speed: 30,
+    autoStart: false,
+    html: true
+  })
+    .tiType('<text style="color: #000; font-size: 100%;">Faculty of Computational Mathematics and Cybernetics</text>');
+}
+
+
+
